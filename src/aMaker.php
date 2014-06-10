4223b12570b1be25e9615fb276fac6a54fe18576
Server <Server@Server-PC>
2014-06-10 21:57:54 +0100

    Fixxed Spelling mistakes by brandon, added consitancy in output.

--- src/aMaker.php
+++ src/aMaker.php
@@ -17,9 +17,9 @@ require_once('curl.php');
 //error_reporting(E_ALL);
 //ini_set('display_errors','on');
 
-$server = "185";
+$server = "183";
 
-echo "[INFO] Fetching server infomation for {$server}".PHP_EOL;
+echo "[INFO] Fetching server infomation for Evony Server # {$server}".PHP_EOL;
 
 $curl = NEW Curl;
 $return = $curl->get("http://{$server}.evony.com/config.xml");
@@ -30,21 +30,20 @@ $port = (int)$feed->port;
 echo "[INFO] Starting loop".PHP_EOL;
 
 while(1){
-	echo "[INFO] Creating random account".PHP_EOL;
+	echo "[INFO] Creating Evony account generated via randomness".PHP_EOL;
 
 	$UID = uniqid();
-	$emailgen = "{$UID}@lol.com";
-	$password = $UID;
-
+	$emailgen = "{$UID}@talentcraft.net";
+	$password = "potato";
 	$s = NEW Socket\Client($address,$port);
-	echo "[INFO] Connecting to {$address}:{$port}",PHP_EOL;
+	echo "[INFO] Connecting to {$address}:{$port} AKA Evony's Fun Land",PHP_EOL;
 
 	$AMF = NEW YaBOB_AMF();
 	$amfHandshake = NEW YaBOB_Handshake();
 	$amfReg = NEW YaBOB_Register;
 	$regInfo = $amfReg->_($emailgen, $password); unset($amfReg);
 	$regData = $AMF->AMFlength($regInfo).$regInfo;
-	echo "[INFO] Waiting for reply".PHP_EOL;
+	echo "[INFO] Waiting for a response".PHP_EOL;
 
 	$s->write($amfHandshake); unset($amfHandshake);
 	$s->write($regData);
@@ -53,17 +52,17 @@ while(1){
 	$in = substr($in, 4);
 	$response = $AMF->destructAMF($in);
 
-	echo "[INFO] Got reply!",PHP_EOL;
+	echo "[INFO] Recieved reply!",PHP_EOL;
 
 	//var_dump($response);
 
 	if(!isset($response->data)){
-		echo "[ERROR] Something has gone wrong, maybe IP blocked for 1 hour".PHP_EOL;
-		exit("[EXIT] Unexpected error");
+		echo "[ERROR] Look's like someone pissed in Evony's Cheerios today; Most likely just 1 hour IP banned by Evony.".PHP_EOL;
+		exit("[EXIT] Unexpected Error");
 	}
 
 	if($response->data['errorMsg'] === "need create player"){
-		echo "[INFO] Creating player using email: {$emailgen} password: {$password}";
+		echo "[INFO] Creating player using generated email: {$emailgen} and password: {$password}";
 		$createplayer = NEW YaBOB_Common_Createnewplayer();
 		$player = $createplayer->_($UID, '','','','','');
 		$createplayer = $AMF->AMFlength($player).$player;
@@ -77,22 +76,22 @@ while(1){
 		}
 
 		echo PHP_EOL;
-		echo "[INFO] Got reply!",PHP_EOL;
+		echo "[INFO] Recieved reply!",PHP_EOL;
 		$in = substr($out, 4);
 		$response = $AMF->destructAMF($in);
 		//var_dump($response);
-
+		
 		//if(isset($response) && @$response->data['errorMsg'] === "NEED CAPTCHA"){
 		While(@$response->data['errorMsg'] === "NEED CAPTCHA"){
-			echo "[WARNING] CAPTCHA BITCH!".PHP_EOL;
+			echo "[WARNING] OH SHIT EVONY GAVE YOU A CAPTCHA!".PHP_EOL;
 			file_put_contents('captcha.png', hex2bin($response->data['captcha']));
 			unset($in);unset($out);
-			echo "[INFO] Opening captcha.png for manual user solving".PHP_EOL;
+			echo "[INFO] Opening captcha.png for manual user solving.".PHP_EOL;
 			exec('captcha.png');
-			echo "[REQUEST] Please solve the captcha: ";
+			echo "[REQUEST] Please solve the captcha; Just type in the captcha text: ";
 			$captcha = fgets(STDIN);
 			$createplayer = NEW YaBOB_Common_Createnewplayer();
-			$player = $createplayer->_($UID,'','','','',trim(str_replace(PHP_EOL, '', $captcha)));
+			$player = $createplayer->_($UID,'Capital','BNH','1','1',trim(str_replace(PHP_EOL, '', $captcha)));
 			$createplayer = $AMF->AMFlength($player).$player;
 			$s->write($createplayer);
 			$in = $s->read();
@@ -102,15 +101,20 @@ while(1){
 			}
 			$in = substr($out, 4);
 			$response = $AMF->destructAMF($in);
+
+			if(!isset($response->data)){
+				echo "[ERROR] Look's like someone pissed in Evony's Cheerios today; Most likely just 1 hour IP banned by Evony.".PHP_EOL;
+				exit("[EXIT] Unexpected Error");
+			}
 		}
 
 		if($response->cmd === "common.createNewPlayer" && @$response->data['msg'] === "success"){
-			echo "[SUCCESS] Player created! Email: {$emailgen} Password: {$password} ";
+			echo "[SUCCESS] Player created with Email: {$emailgen} and Password: {$password} ";
 			$x = intval($response->data['player']['castles'][0]['fieldId'] % 800);
 			$y = intval($response->data['player']['castles'][0]['fieldId'] / 800);
 			echo "Castle coords: {$x},{$y} PlayerID: {$response->data['player']['castles'][0]['id']}".PHP_EOL;
 		}else if(isset($response) && $response->data['errorMsg'] === "All Valleys are already occupied, please choose another state. ") {
-			echo "[INFO] Server full".PHP_EOL;
+			echo "[INFO] No flats exist for your city to lay it's ass.".PHP_EOL;
 			exit("[EXIT] Server returned: {$response->data['errorMsg']}");
 		} else {
 			exit("[EXIT] Unknown error: {$response->data['errorMsg']}");
@@ -139,18 +143,23 @@ while(1){
 	}
 
 	echo PHP_EOL;
-	echo "[INFO] Got reply!",PHP_EOL;
+	echo "[INFO] Recieved reply!",PHP_EOL;
 
 	$out = substr($out, 4);
 	$response = $AMF->destructAMF($out);
+	
+	if(!isset($response->data)){
+		echo "[ERROR] Look's like someone pissed in Evony's Cheerios today; most likely just 1 hour IP banned by Evony.".PHP_EOL;
+		exit("[EXIT] Unexpected Error");
+	}
 
 	if(@$response->data['msg'] === "login success"){
-		echo "[SUCCESS] server returned: {$response->data['msg']}".PHP_EOL;
+		echo "[SUCCESS] Server returned: {$response->data['msg']}".PHP_EOL;
 		echo "[INFO] Saving player data".PHP_EOL;
 		$playerformat = "SERVER: {$server}, EMAIL: {$emailgen}, PASSWORD: {$password}, PLAYERNAME: {$UID}, COORD: {$x},{$y}".PHP_EOL;
 		file_put_contents("accounts.txt", $playerformat, FILE_APPEND);
 	}else if(@$response->data['errorMsg'] === "need create player"){
-		echo "[WARNING] Player mite of failed to create!".PHP_EOL;
+		echo "[WARNING] Account creation failed!".PHP_EOL;
 	} else {
 		var_dump($response);
 		exit("[EXIT] Server returned: {$response->data['errorMsg']}");
